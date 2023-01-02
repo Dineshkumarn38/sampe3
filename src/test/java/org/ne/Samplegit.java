28a70066dd0e1f9ef24843e04ebcb7732308663a
Dineshkumarn38 <dineshkumarn38@gmail.com>
2023-01-02 14:47:55 +0530

    sample 3 dinesh

---
+++ src/test/java/org/ne/Samplegit.java
@@ -0,0 +1,13 @@
+package org.ne;
+
+public class Samplegit {
+	public void dinesh() {
+		System.out.println("dinesh");
+		
+
+	}
+	public static void main(String[] args) {
+		Samplegit d = new Samplegit();
+		d.dinesh();
+	}
+}
